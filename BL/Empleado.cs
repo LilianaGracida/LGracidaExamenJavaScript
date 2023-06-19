7514feb2a5358b5f6382fc768c54fa8734fd82f0
LilianaGracida <lgbrioso@gmail.com>
2023-06-19 13:21:26 -0600

    Add project files.

---
+++ BL/Empleado.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Empleado
+    {
+        public static ML.Result Add(ML.Empleado empleado)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.ExamenJavaScriptEntities context = new DL.ExamenJavaScriptEntities())
+                {
+                    var query = context.CineAdd(empleado.Nombre, empleado.Direccion, empleado.Venta, empleado.Zona.IdZona);
+
+                    if (query >= 1)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se ha podido realizar el insert";
+                    }
+                    result.Correct = true;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+    }
+}
