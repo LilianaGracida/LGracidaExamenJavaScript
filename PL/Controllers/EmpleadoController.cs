73e01c72f3981d01e18921c705baa2ca47590749
LilianaGracida <lgbrioso@gmail.com>
2023-06-21 14:07:08 -0600

    Completo

--- PL/Controllers/EmpleadoController.cs
+++ PL/Controllers/EmpleadoController.cs
@@ -13,44 +13,5 @@ namespace PL.Controllers
         {
             return View();
         }
-
-        [HttpGet]
-        public ActionResult Form()
-        {
-            return View(new ML.Empleado());
-        }
-
-        [HttpGet]
-        public JsonResult Get()
-        {
-            ML.Result result = BL.Empleado.GetAll();
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public JsonResult GetById(int IdEmpleado)
-        {
-            ML.Result result = BL.Empleado.GetById(IdEmpleado);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public JsonResult Add(ML.Empleado empleado)
-        {
-            ML.Result result = BL.Empleado.Add(empleado);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public JsonResult Delete(int IdEmpleado)
-        {
-            ML.Empleado empleado = new ML.Empleado();
-            empleado.IdEmpleado = IdEmpleado;
-            ML.Result result = BL.Empleado.Delete(empleado);
-
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
     }
 }
